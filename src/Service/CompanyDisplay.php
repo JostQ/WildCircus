35ab5c725514b5bab99a1d2800a4321d47b475ee
dremsis <JostQ>
2019-07-16 18:52:41 +0200

    homepage done

--- src/Service/CompanyDisplay.php
+++ src/Service/CompanyDisplay.php
@@ -4,14 +4,25 @@
 namespace App\Service;
 
 use App\Repository\CompanyRepository;
+use App\Repository\MediaRepository;
+use App\Repository\TypeRepository;
 use Twig\Environment;
 
 class CompanyDisplay
 {
+    private $company;
+    private $logo;
 
-    public function __construct(CompanyRepository $companyRepository, Environment $twig)
+    public function __construct(CompanyRepository $companyRepository, MediaRepository $mediaRepository, TypeRepository $typeRepository)
     {
-        return $companyRepository->findBy([]);
+        $this->company = $companyRepository->findBy([])[0];
+        $type = $typeRepository->findOneBy(['name' => 'logoCompany']);
+        $this->logo = $mediaRepository->findOneBy(['type' => $type]);
+    }
+
+    public function getLogo()
+    {
+        return $this->logo;
     }
 
 }
