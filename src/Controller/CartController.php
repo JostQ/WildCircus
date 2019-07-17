1388da2ee50f04bab820a65c775d08a6f653dc24
dremsis <JostQ>
2019-07-17 10:32:07 +0200

    routing with controller

---
+++ src/Controller/CartController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/cart", name="cart_")
+ * @IsGranted("ROLE_USER")
+ */
+class CartController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('cart/index.html.twig', [
+            'controller_name' => 'CartController',
+        ]);
+    }
+}
