ea9f919e1eb847f2b7a6b0a39672024a0ef33605
dremsis <JostQ>
2019-07-19 09:04:11 +0200

    service uploader

---
+++ src/Service/Uploader.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Service;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
+class Uploader
+{
+    private $targetDirectory;
+
+    public function __construct($targetDirectory)
+    {
+        $this->targetDirectory = $targetDirectory;
+    }
+
+    public function upload(UploadedFile $file) : string
+    {
+        $fileName = md5(uniqid()) . '.' . $file->guessExtension();
+        try {
+            $file->move($this->getTargetDirectory(), $fileName);
+        } catch (FileException $e) {
+        }
+        return $fileName;
+    }
+
+    public function getTargetDirectory()
+    {
+        return $this->targetDirectory;
+    }
+}
